e0ed66fd663b9650295c476aff11a13669998aab
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-09-06 06:35:06 +0300

    Add files via upload

---
+++ DesignPatterns/1_SingletonDesignPattern/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        public int a = 0;
+    }
+}
---
+++ DesignPatterns/1_SingletonDesignPattern/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Main : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            GameManager.Instance.a = 5;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log( GameManager.Instance.a );
+        }
+    }
+}
