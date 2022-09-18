6d25f7dbb878650dd57e474bc7b177fccd83b10a
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-09-18 09:11:14 +0300

    Create 16_BeautifulDays.cs

---
+++ HackerRankSolutions/16_BeautifulDays.cs
@@ -0,0 +1,76 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k)
+    {
+        int inversedNumber = 0, number=0, last = 0, diff = 0, count = 0;
+
+        for (int t = i; t <= j; t++)
+        {
+            number = t;
+            while (number > 0)
+            {
+                last = number % 10;
+                if( last != 0 )
+                    inversedNumber = inversedNumber * 10 + last;
+                number /= 10;
+            }
+
+            diff = Math.Abs(inversedNumber - t);
+            if (diff % k == 0)
+            {
+                count++;
+            }
+            inversedNumber = 0;
+        }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int i = Convert.ToInt32(firstMultipleInput[0]);
+
+        int j = Convert.ToInt32(firstMultipleInput[1]);
+
+        int k = Convert.ToInt32(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
