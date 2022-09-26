67066b6f7a825ffa04124fe35013878c9ae67f25
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-09-26 22:00:24 +0300

    Create 19_FindDigit.cs

---
+++ HackerRankSolutions/19_FindDigit.cs
@@ -0,0 +1,65 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n)
+    {
+        int lastNumber, temp = n, count =0;
+        
+        while( temp > 0 )
+        {
+            lastNumber = temp % 10;
+            if( lastNumber != 0 && temp % lastNumber == 0)
+            {
+                count++;
+            }
+            
+            temp = temp/10;
+        }
+        
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.findDigits(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
