bd9cc398e878203a0d6502595947cf78f30b0af2
rumeysalyk <layikrumeysa@gmail.com>
2022-09-28 00:14:24 +0300

    Intro scene is created and buttonAnimations are created

---
+++ DesignPatternGameApplications/SnakeGame/Assets/Scripts/IntroductionAnimationProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class IntroductionAnimationProvider : MonoBehaviour
+    {
+        private int k_onMouseEnter = Animator.StringToHash( "OnMouseEnter" );
+
+        [SerializeField] private Animator m_animator; 
+
+        private void OnMouseEnter()
+        {
+            m_animator.SetBool( k_onMouseEnter, true );
+        }
+
+        private void OnMouseExit()
+        {
+            m_animator.SetBool( k_onMouseEnter, false );
+        }
+    }
+}
