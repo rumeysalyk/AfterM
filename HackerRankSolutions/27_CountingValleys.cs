761335f5642c0e62f3fcca9c0dbea515329704d0
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-10-02 15:42:45 +0300

    Create 27_CountingValleys.cs

---
+++ HackerRankSolutions/27_CountingValleys.cs
@@ -0,0 +1,77 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+        public static int countingValleys(int steps, string path)
+        {
+            int position = 0, countValley = 0;
+            bool isSeaLevel = true;
+            
+            for (int i = 0; i < steps; i++)
+            {
+                if (path[i] == 'D')
+                {
+                    position--;
+                }
+                else if (path[i] == 'U')
+                {
+                    position++;
+                }
+                
+                if ( position < 0 && isSeaLevel == true )
+                {
+                    countValley++;
+                    isSeaLevel = false;
+                }
+                if( position == 0 )
+                {
+                    isSeaLevel = true;
+                }
+
+            }
+
+            return countValley;
+        }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
