75e5662d434b1b90dcf4bc03c59afa4fc3ca4590
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-10-04 01:58:28 +0300

    Create 30_Pangrams.cs

---
+++ HackerRankSolutions/30_Pangrams.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+    {
+        bool isAllExist = true;
+        s= s.ToLower();
+        for( char i='a'; i< 'z'; i++ )
+        {
+            if( !s.Contains(i) )
+            {
+                isAllExist = false;
+                break;
+            }
+            else
+            {
+                isAllExist = true;
+            } 
+        }
+        
+        if( isAllExist )
+        {
+            return "pangram";
+        }
+        else
+        {
+            return "not pangram";
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
