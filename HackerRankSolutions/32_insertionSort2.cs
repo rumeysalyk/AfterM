8908ca992afa21b0756da4e536d3bf7876d82662
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-10-07 00:11:48 +0300

    Create 32_insertionSort2.cs

---
+++ HackerRankSolutions/32_insertionSort2.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'insertionSort2' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort2(int n, List<int> arr)
+    {
+        int temp = 0;
+        for( int i=1; i<arr.Count; i++ )
+        {
+            for( int j=0; j< i ; j++ )
+            {
+                if(arr[i] < arr[j])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            
+            foreach( var t in arr )
+            {
+                Console.Write(t + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.insertionSort2(n, arr);
+    }
+}
