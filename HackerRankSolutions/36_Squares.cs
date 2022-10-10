14a80dc9985ed0012b40b88042f69a8b2e978697
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2022-10-10 22:25:28 +0300

    Create 36_Squares.cs

---
+++ HackerRankSolutions/36_Squares.cs
@@ -0,0 +1,70 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b)
+    {
+        int count =0;
+        int x = (int)MathF.Ceiling(MathF.Sqrt(a));
+        int y = (int)MathF.Floor(MathF.Sqrt(b));
+        
+        for( int i=x; i<=y; i++ )
+        {
+            if( i*i>= a && i*i<=b )
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(firstMultipleInput[0]);
+
+            int b = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.squares(a, b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
