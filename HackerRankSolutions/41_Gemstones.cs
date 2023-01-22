2d1a3ac3622719f662a700c4a8ca19f78a54db68
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2023-01-22 17:41:06 +0300

    Create 41_Gemstones.cs

---
+++ HackerRankSolutions/41_Gemstones.cs
@@ -0,0 +1,73 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gemstones' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY arr as parameter.
+     */
+
+    public static int gemstones(List<string> arr)
+    {
+        int counter = 0, count; 
+        for( char i = 'a'; i<='z'; i++ )
+        {
+            count = 0;
+            for( int j=0; j< arr.Count; j++ )
+            {
+                if( arr[j].Contains(i) )
+                {
+                    count++;
+                }
+            }
+            
+            if( count == arr.Count )
+            {
+                counter++;
+            }
+        }
+        
+        return counter;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> arr = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string arrItem = Console.ReadLine();
+            arr.Add(arrItem);
+        }
+
+        int result = Result.gemstones(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
