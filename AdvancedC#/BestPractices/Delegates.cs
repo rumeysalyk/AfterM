18c5dc14f3e15c9579fdcf27be2cb014a2a77b17
rumeysalyk <layikrumeysa@gmail.com>
2023-01-24 05:40:32 +0300

    Assigned second method to be delegated

--- AdvancedC#/BestPractices/Delegates.cs
+++ AdvancedC#/BestPractices/Delegates.cs
@@ -1,19 +1,32 @@
+using UnityEngine;
 
-public class TestDelegate
+namespace Assets
 {
-    public delegate void Tester( int num );
-
-    public Tester TestDelegateObject;
-
-    public TestDelegate()
+    public class TestDelegate : MonoBehaviour
     {
-        TestDelegateObject = ExampleFunction;
-        TestDelegateObject( 1254 );
-        TestDelegateObject?.Invoke( 1254 );
-    }
+        public delegate void Tester(int num);
 
-    public void ExampleFunction( int a_num )
-    {
-        Console.WriteLine( "Tested delegate" + a_num );
+        public Tester TestDelegateObject;
+
+        public TestDelegate()
+        {
+            TestDelegateObject = ExampleFunction;
+
+            TestDelegateObject(1254);
+            TestDelegateObject?.Invoke(1254); //Second call method
+
+            TestDelegateObject = ExampleFunction2;
+            TestDelegateObject(1478);
+        }
+
+        public void ExampleFunction(int a_num)
+        {
+            UnityEngine.Debug.LogError("Number1: " + a_num);
+        }
+
+        public void ExampleFunction2(int a_num)
+        {
+            UnityEngine.Debug.LogError("Number2: " + a_num);
+        }
     }
 }
