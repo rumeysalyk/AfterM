a850813c3701513d4948144b81ea85cdefe5d4bf
rumeysalyk <layikrumeysa@gmail.com>
2023-01-28 10:53:44 +0300

    Event is defined as static

---
+++ AdvancedC#/BestPractices/A1/StaticEvents/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurant restaurant = new Restaurant();
+
+            Reception reception = new Reception();
+
+            Table table = new Table();
+
+            restaurant.AddCustomer( "Nazım Hikmet" );
+
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
---
+++ AdvancedC#/BestPractices/A1/StaticEvents/Restaurant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Restaurant
+    {
+        private readonly List<string> m_customers = new List<string>();
+        public static event Action<string> NewCustomer; 
+        public void AddCustomer( string a_customerName )
+        {
+            m_customers.Add( a_customerName );
+            NewCustomer?.Invoke( a_customerName );
+        }
+    }
+    
+    public class Reception
+    {
+        public Reception()
+        {
+            Restaurant.NewCustomer += ChargeWaiter;
+        }
+        
+        public void ChargeWaiter( string a_customerName )
+        {
+            Console.WriteLine( $"Charged a waiter for {a_customerName}" );
+        }
+    }
+
+    public class Table
+    {
+
+        public Table()
+        {
+            Restaurant.NewCustomer += BookTable;
+        }
+        public void BookTable( string a_customerName )
+        {
+            Console.WriteLine( $"{ a_customerName} booked a table" );
+        }
+    }
+
+
+}
