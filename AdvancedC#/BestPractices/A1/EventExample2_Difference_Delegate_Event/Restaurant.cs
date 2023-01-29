8d4a1c2501d207e65a41277a82d077b81456f371
rumeysalyk <layikrumeysa@gmail.com>
2023-01-29 23:14:02 +0300

    Delegates can use assign operator

---
+++ AdvancedC#/BestPractices/A1/EventExample2_Difference_Delegate_Event/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Restaurant
+    {
+        public Action<string> NewCustomer; 
+        public void AddCustomer( string a_customerName )
+        {
+            NewCustomer?.Invoke( a_customerName );
+        }
+    }
+    
+    public class Reception
+    {
+        public void ChargeWaiter( string a_customerName )
+        {
+            Console.WriteLine( $"Charged a waiter for {a_customerName}" );
+        }
+    }
+
+    public class Table
+    {
+        public void BookTable( string a_customerName )
+        {
+            Console.WriteLine( $"{ a_customerName} booked a table" );
+        }
+    }
+
+
+}
